02635611c128db65d17056d597e370de9c74720e
GNghost <wlvud6902@gmail.com>
2022-02-18 00:27:43 +0900

    update hook

--- hook_FT.f90
+++ hook_FT.f90
@@ -1,24 +1,29 @@
 program hook_FT
 integer*8 i, imax, j, jmax, k, kmax
-real*8 t, dt, x, dx, v, dv, om, d_om, om0, x(imax), sum_re, sum_im
+real*8 t, dt, x, dx, v, dv, om, d_om, om0, sum_re, sum_im
 parameter(imax = 1e+5, jmax = 1e+5, kmax = 1e+5, dt = 1.e-3, d_om = 1.e-3, om0 = 1.)
+real*8 x_vec(imax)
 
 x = 0.; v = 1.
 
 do i = 1, imax
 
-dx = v ; dv = -om0**2 * x
+dx = v; dv = -om0**2 * x
 
 x_vec(i) = x
-
-x = x + dx * dt
+print*, x_vec(i)
+x = x + dx * dt; v = v + dv * dt
 
 enddo
 
+do k = 1, kmax
+
 do j = 1, jmax
+om = j * d_om; sum_re = 0.; sum_im = 0.
+enddo
 
-om = j * d_om
+sum_re = sum_re + x_vec(i) * cos(om * k * dt) * dt; sum_im = sum_im -x_vec(i) * sin(om * k *dt) * dt
 
-sum_im = sum_0.
+enddo
 
 end
