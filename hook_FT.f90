aeec2ba2abb19c1cb74688e098aa3fa910d3b213
JiPang22 <wlvud6902@gmail.com>
2022-02-17 23:10:26 +0900

    add ft

--- hook_FT.f90
+++ hook_FT.f90
@@ -3,5 +3,22 @@ integer*8 i, imax, j, jmax, k, kmax
 real*8 t, dt, x, dx, v, dv, om, d_om, om0, x(imax), sum_re, sum_im
 parameter(imax = 1e+5, jmax = 1e+5, kmax = 1e+5, dt = 1.e-3, d_om = 1.e-3, om0 = 1.)
 
+x = 0.; v = 1.
+
+do i = 1, imax
+
+dx = v ; dv = -om0**2 * x
+
+x_vec(i) = x
+
+x = x + dx * dt
+
+enddo
+
+do j = 1, jmax
+
+om = j * d_om
+
+sum_im = sum_0.
 
 end
