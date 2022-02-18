da455f713776cc20d55e1ddc9c864572444b90e6
jp <wlvud6902@gmail.com>
2022-02-18 16:45:40 +0900

    add sh

--- hook_FT.f90
+++ hook_FT.f90
@@ -1,28 +1,42 @@
 program hook_FT
-integer*8 i, imax, j, jmax, k, kmax
-real*8 t, dt, x, dx, v, dv, om, d_om, om0, sum_re, sum_im
-parameter(imax = 1e+5, jmax = 1e+5, kmax = 1e+5, dt = 1.e-3, d_om = 1.e-3, om0 = 1.)
-real*8 x_vec(imax)
+integer*8 i, imax, k, kmax
+real*8 t, dt, x, dx, v, dv, om, d_om, om0, sum_re, sum_im, xom
+parameter(imax = 3.e+4, kmax = 1.e+2, dt = 1.e-3, d_om = 1.e-3, om0 = 1.)
+real x_t(imax), x_om(kmax)
 
-x = 0.; v = 1.
+open(1,file='xt')
+open(2,file='vt')
+open(3,file='vx')
+!open(4,file='xw')
+
+x = 0.; v = 1.; xom = 0.
+!sum_re = 0.; sum_im = 0.
 
 do i = 1, imax
 
+write(1,*) t, x
+write(2,*) t, v
+write(3,*) x, v
+
+!    do k = 1, kmax
+!    write(4,*) om, xom
+ !   om = k * d_om 
+  
+!    sum_re = sum_re + x * cos(om * t) * dt
+ !   sum_im = sum_im -x * sin(om * t) * dt
+  
+  !  x_om = sqrt(sum_re**2 + sum_im**2)
+   ! x_om(k) = xom
+ 
+!    enddo
+
+t = i * dt; x_t(i) = x
+
 dx = v; dv = -om0**2 * x
 
-x_vec(i) = x
-print*, x_vec(i)
 x = x + dx * dt; v = v + dv * dt
 
-enddo
-
-do k = 1, kmax
-
-do j = 1, jmax
-om = j * d_om; sum_re = 0.; sum_im = 0.
-enddo
-
-sum_re = sum_re + x_vec(i) * cos(om * k * dt) * dt; sum_im = sum_im -x_vec(i) * sin(om * k *dt) * dt
+print*, "step ", i
 
 enddo
 
