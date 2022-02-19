c5e4625c435fc6f2b899cf4c51cd6f7e79b2f984
jp <wlvud6902@gmail.com>
2022-02-19 20:42:45 +0900

    add FT

--- hook_FT.f90
+++ hook_FT.f90
@@ -1,7 +1,7 @@
 program hook_FT
 integer*8 i, imax, k, kmax, n, nmax
 real*8 dt, x, dx, v, dv, d_om, om0, sum_re, sum_im
-parameter(imax = 3.e+4, kmax = 3.e+4, nmax = 3.e+4, dt = 1.e-3, d_om = 1.e-3, om0 = 1.)
+parameter(imax = 3e+4, kmax = 3e+4, nmax = 3e+4, dt = 1.e-3, d_om = 1.e-3, om0 = 1.)
 real x_t(imax)
 
 open(1,file='xt')
@@ -17,19 +17,15 @@ write(2,*) i * dt, v
 write(3,*) x, v
 x_t(i) = x; dx = v; dv = -om0**2 * x
 x = x + dx * dt; v = v + dv * dt
-print*, "step...", "i =", i
 enddo
 
 do n = 1, nmax ! n is omega index number
 write(4,*) n * d_om, sqrt(sum_re**2 + sum_im**2)
-
 do k = 1, kmax ! k is sumation loop index number 
 sum_re = sum_re + x_t(k) * cos(n * d_om * k * dt) * dt
 sum_im = sum_im -x_t(k) * sin(n * d_om * k * dt) * dt
 enddo
-
-print*, "step...", "n =", n
-print*, n * d_om, sqrt(sum_re**2 + sum_im**2)
+print*, "step... ", n
 enddo
 
 stop
