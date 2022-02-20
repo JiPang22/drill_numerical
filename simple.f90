6236013d59eee5b6818e0929440d79727e117fc9
JiPang22 <wlvud6902@gmail.com>
2022-02-21 08:45:23 +0900

    update simple

--- simple.f90
+++ simple.f90
@@ -1,29 +1,33 @@
 program simple
-integer i, n
-real dt, x, dx, v, dv, dw, om0, sum_re, sum_im
-parameter(tmax = 10, wmax = 10, pmax = 10, dt = 1.e-2, dw = 1.e-3, om0 = 1., period = 2 * acos(-1.)/om0)
-real x_t(int(tmax/dt))
-
+integer i, n, tmax, wmax, pmax, imax, nmax
+real dt, x, dx, v, dv, dw, om0, sum_re, sum_im , t
+parameter(wmax = 100, pmax = 100, imax =1e+3, nmax = 1e+3, om0 = 10., period = 2 * acos(-1.)/om0)
+parameter(tmax = int(pmax * period))
+parameter(dt = tmax/imax, dw = wmax/nmax)
+real x_t(imax)
 open(1,file='xt')
 open(2,file='vt')
 open(3,file='vx')
 open(4,file='xw')
 
-x = 0.; v = 1.
+t = 0.; x = 0.; v = 1.
 
-do i = 1, int(tmax/dt) ! "i" is time series index
-write(1,*) i * dt, x
-write(2,*) i * dt, v
+do i = 1, imax ! "i" is time series index
+t = i * dt
+write(1,*) t, x
+write(2,*) t, v
 write(3,*) x, v
-x_t(i) = x; dx = v; dv = -x * om0 ** 2
+x_t(i) = x
+
+dx = v; dv = -x * om0 ** 2
 x = x + dx * dt; v = v + dv * dt
 enddo
 
-do n = 1, int(wmax/dw) ! n is omega index
+do n = 1, nmax ! n is omega index
 write(4,*) n * dw, sqrt(sum_im**2 + sum_re**2)
-do i = 1, int(tmax/dt) ! "i" is time series index of period summation
-sum_re = sum_re + x_t(i) * cos(n * d_om * i * dt) * dt
-sum_im = sum_im -x_t(i) * sin(n * d_om * i * dt) * dt
+do i = 1, imax ! "i" is time series index of period summation
+sum_re = sum_re + x_t(i) * cos(n * d_om * t) * dt
+sum_im = sum_im -x_t(i) * sin(n * d_om * t) * dt
 enddo
 print*, "step... ", n 
 enddo
