6c368db3f381d43853a9ddafda8d30bd8e0467a6
JiPang22 <wlvud6902@gmail.com>
2022-02-21 00:08:16 +0900

    update simple

--- simple.f90
+++ simple.f90
@@ -1,8 +1,8 @@
-program hook_FT
-integer*8 i, imax, k, kmax, n, nmax
-real*8 dt, x, dx, v, dv, d_om, om0, sum_re, sum_im
-parameter(imax = 3e+4, kmax = 3e+4, nmax = 1e+4, dt = 1.e-4, d_om = 1.e-2, om0 = 20.)
-real x_t(imax)
+program simple
+integer i, n
+real dt, x, dx, v, dv, dw, om0, sum_re, sum_im
+parameter(tmax = 10, wmax = 10, pmax = 10, dt = 1.e-2, dw = 1.e-3, om0 = 1., period = 2 * acos(-1.)/om0)
+real x_t(int(tmax/dt))
 
 open(1,file='xt')
 open(2,file='vt')
@@ -11,21 +11,21 @@ open(4,file='xw')
 
 x = 0.; v = 1.
 
-do i = 1, imax
+do i = 1, int(tmax/dt) ! "i" is time series index
 write(1,*) i * dt, x
 write(2,*) i * dt, v
 write(3,*) x, v
-x_t(i) = x; dx = v; dv = -om0**2 * x
+x_t(i) = x; dx = v; dv = -x * om0 ** 2
 x = x + dx * dt; v = v + dv * dt
 enddo
 
-do n = 1, int(2 * acos(-1.)/om0) ! n is omega index number
-write(4,*) n * d_om, sqrt(sum_im**2 + sum_re**2)
-do k = 1, kmax ! k is sumation loop index number 
-sum_re = sum_re + x_t(k) * cos(n * d_om * k * dt) * dt
-sum_im = sum_im -x_t(k) * sin(n * d_om * k * dt) * dt
+do n = 1, int(wmax/dw) ! n is omega index
+write(4,*) n * dw, sqrt(sum_im**2 + sum_re**2)
+do i = 1, int(tmax/dt) ! "i" is time series index of period summation
+sum_re = sum_re + x_t(i) * cos(n * d_om * i * dt) * dt
+sum_im = sum_im -x_t(i) * sin(n * d_om * i * dt) * dt
 enddo
-print*, "step... ", n
+print*, "step... ", n 
 enddo
 
 stop
